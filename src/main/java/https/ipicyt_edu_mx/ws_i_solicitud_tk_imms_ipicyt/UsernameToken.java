ad9e4a8bca9988fd79477f50e77d260148024330
Eddy Reynaldo López Olivera <erlopezo@scjn.gob.mx>
2021-01-20 08:22:28 -0600

    Error de impl

--- src/main/java/https/ipicyt_edu_mx/ws_i_solicitud_tk_imms_ipicyt/UsernameToken.java
+++ src/main/java/https/ipicyt_edu_mx/ws_i_solicitud_tk_imms_ipicyt/UsernameToken.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
 // Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2021.01.18 a las 04:10:12 PM CST 
+// Generado el: 2021.01.20 a las 12:54:41 AM CST 
 //
 
 
