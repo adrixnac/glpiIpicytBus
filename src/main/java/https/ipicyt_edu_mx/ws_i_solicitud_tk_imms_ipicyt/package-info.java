782f82beb91b8cd085c85b42e925d9fb91131eb5
erlopezo <erlopezo@scjn.gob.mx>
2021-02-05 14:11:27 -0600

    Debug

--- src/main/java/https/ipicyt_edu_mx/ws_i_solicitud_tk_imms_ipicyt/package-info.java
+++ src/main/java/https/ipicyt_edu_mx/ws_i_solicitud_tk_imms_ipicyt/package-info.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
 // Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2021.01.31 a las 07:40:09 PM CST 
+// Generado el: 2021.02.05 a las 11:29:42 AM CST 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "https://ipicyt.edu.mx/ws_i_solicitud_tk_imms_ipicyt", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
