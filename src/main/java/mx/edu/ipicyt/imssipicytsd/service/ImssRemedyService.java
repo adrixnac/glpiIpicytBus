6ea3e631dff342ffa37252ae2ba18110e7cecdd9
Eddy R. López Olivera <eddylopezolivera@MacBook-Pro-de-Eddy.local>
2021-03-24 11:26:28 -0600

    Logs

--- src/main/java/mx/edu/ipicyt/imssipicytsd/service/ImssRemedyService.java
+++ src/main/java/mx/edu/ipicyt/imssipicytsd/service/ImssRemedyService.java
@@ -3,12 +3,17 @@ package mx.edu.ipicyt.imssipicytsd.service;
 import https.ipicyt_edu_mx.ws_i_solicitud_tk_imms_ipicyt.InputMapping1;
 import https.ipicyt_edu_mx.ws_i_solicitud_tk_imms_ipicyt.OutputMapping1;
 import mx.edu.ipicyt.imssipicytsd.domain.Ticket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 
 public class ImssRemedyService extends WebServiceGatewaySupport {
+    private final Logger log = LoggerFactory.getLogger(ImssRemedyService.class);
     public OutputMapping1 getImss(Ticket ticket){
+        log.debug("--ImssRemedyService-- {}", ticket);
         InputMapping1 inputMapping1 = new InputMapping1();
         inputMapping1.setNombreProducto(ticket.getNombreProducto());
+        log.debug("--inputMapping1-- {}", inputMapping1.toString());
         return (OutputMapping1) getWebServiceTemplate().marshalSendAndReceive(inputMapping1);
 
     }
--- src/main/java/mx/edu/ipicyt/imssipicytsd/soapclient/SoapClientConfig.java
+++ src/main/java/mx/edu/ipicyt/imssipicytsd/soapclient/SoapClientConfig.java
@@ -3,12 +3,15 @@ package mx.edu.ipicyt.imssipicytsd.soapclient;
 import mx.edu.ipicyt.imssipicytsd.domain.Ticket;
 import mx.edu.ipicyt.imssipicytsd.service.ImssRemedyService;
 import org.opensaml.xmlsec.signature.J;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
 @Configuration
 public class SoapClientConfig {
+    private final Logger log = LoggerFactory.getLogger(SoapClientConfig.class);
     @Bean
     public Jaxb2Marshaller marshaller(){
         Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
@@ -18,6 +21,7 @@ public class SoapClientConfig {
 
     @Bean
     public ImssRemedyService imssRemedyService(Jaxb2Marshaller jaxb2Marshaller){
+        log.debug("-- imssRemedyService --");
         ImssRemedyService imssRemedyService = new ImssRemedyService();
         imssRemedyService.setDefaultUri("http://172.16.162.38//services/ARService?server=remedy&amp;webService=PCT_Creacion_WS");
         imssRemedyService.setMarshaller(jaxb2Marshaller);
