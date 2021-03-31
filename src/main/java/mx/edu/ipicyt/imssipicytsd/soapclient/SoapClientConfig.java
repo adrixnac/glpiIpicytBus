2aae18440956250ffd93b1f07bb5e04328ab1e81
Eddy R. López Olivera <eddylopezolivera@MacBook-Pro-de-Eddy.local>
2021-03-31 15:25:45 -0600

    Template

--- src/main/java/mx/edu/ipicyt/imssipicytsd/soapclient/SoapClientConfig.java
+++ src/main/java/mx/edu/ipicyt/imssipicytsd/soapclient/SoapClientConfig.java
@@ -8,13 +8,24 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.ws.client.core.WebServiceTemplate;
+import org.springframework.ws.soap.saaj.SaajSoapMessageFactory;
+
+import javax.xml.soap.MessageFactory;
+import javax.xml.soap.SOAPException;
 
 @Configuration
 public class SoapClientConfig {
     private final Logger log = LoggerFactory.getLogger(SoapClientConfig.class);
     @Bean
-    public Jaxb2Marshaller marshaller(){
+    public Jaxb2Marshaller marshaller() throws SOAPException {
+        SaajSoapMessageFactory messageFactory = new SaajSoapMessageFactory(
+            MessageFactory.newInstance());
+        messageFactory.afterPropertiesSet();
         Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
+        WebServiceTemplate webServiceTemplate = new WebServiceTemplate(messageFactory);
+        webServiceTemplate.setMarshaller(jaxb2Marshaller);
+        webServiceTemplate.afterPropertiesSet();
         jaxb2Marshaller.setContextPath("https.ipicyt_edu_mx.ws_i_solicitud_tk_imms_ipicyt");
         return jaxb2Marshaller;
     }
